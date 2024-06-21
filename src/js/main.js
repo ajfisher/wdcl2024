4f194876b533a4d9a71c87d25481ae202cc3df38
ajfisher <ajfisher.td@gmail.com>
2024-06-21 21:49:39 +1000

    feat: add icon styles and print status init

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,20 @@ import Notes from 'reveal.js/plugin/notes/notes.esm.js';
 import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/black.css';
 import '../css/theme/ajfisher.css';
+import '../css/fontawesome/css/fontawesome.min.css';
+import '../css/fontawesome/css/brands.min.css';
+
+let showNotes = false;
+if( window.location.search.match( /print-pdf/gi ) ) {
+  showNotes = true;
+}
 
 // Initialize Reveal.js
 let deck = new Reveal({
   width: 1280,
   height: 720,
 
+  center: false,
   controls: false,
   controlsTutorial: false,
   progress: true,
@@ -17,13 +25,20 @@ let deck = new Reveal({
   transition: 'fade',
   backgroundTransition: 'fade',
 
+  totalTime: 1740,
+
   plugins: [
     Markdown,
     Notes
-  ]
+  ],
+
+  showNotes,
+  pdfSeparateFragments: false
 });
+
 deck.initialize().then(() => {
   console.log('Reveal.js is inited');
+  console.log(`Notes on: ${showNotes}`);
 }).catch(error => {
   console.log('error init', error);
 });
